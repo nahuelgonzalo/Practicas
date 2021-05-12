654cf68d5b00f8d6ba6495135a845dc11c64b7c6
Nahuel Gonzalo Girón <giron.nahuel@gmail.com>
2021-05-12 00:15:48 -0300

    Practica 2

--- src/ejercicioNro1/Main.java
+++ src/ejercicioNro1/Main.java
@@ -13,7 +13,7 @@ public class Main {
 			
 			Producto producto1 = new Producto();
 			
-			Producto producto2 = new Producto();
+			
 			
 			
 			
@@ -36,56 +36,83 @@ public class Main {
 			
 				leerComprador(scan, unComprador);
 				
-				
-				
-				leerProducto(scan, producto1);
+				imprimirPrimero(unComprador, dia, mes, annio);
+				procesarVentas(scan, producto1);	
 					
 					
-					
-			//Producto2		
-					
-				System.out.println("Producto 2:");
-					
-				leerProducto(scan, producto2);
-					
+			
 					
 					
 					
 					
 			
-				System.out.println("*************************************************************************************");
 				
-				System.out.println("Fecha:"+dia+"-"+mes+"-"+annio);
 				
-				System.out.println("*************************************************************************************");
 				
-				System.out.println("Nombre: "+unComprador.getRazonSocial());
-				System.out.println("Domicilio: "+unComprador.getDomicilio());
 				
-				System.out.println("*************************************************************************************");
 				
-				System.out.println("Cant.  | Descripcion		|P.unit					 |P.total");
-				System.out.println("-------------------------------------------------------------------------------------");
 				
-				System.out.println(+producto1.getCantidad() +"      | "+producto1.getDescripcion()+"  		    |  "+producto1.getPrecio()+"			  |  "+producto1.getCantidad()*producto1.getPrecio());
-				
-				System.out.println("-------------------------------------------------------------------------------------");
-				
-				System.out.println(+producto2.getCantidad() +"      | "+producto2.getDescripcion()+"  		    |  "+producto2.getPrecio()+"			  |  "+producto2.getCantidad()*producto2.getPrecio());
-				
-				System.out.println("-------------------------------------------------------------------------------------");
-				
-				System.out.println("IVA: 21%");
-				System.out.println("Subtotal (sin IVA):"+((producto1.getCantidad()*producto1.getPrecio()) + (producto2.getCantidad()*producto2.getPrecio())));
-				System.out.println("Total:"+((producto1.getCantidad()*producto1.getPrecio() + producto2.getCantidad()*producto2.getPrecio()) * 0.21 + producto1.getCantidad()*producto1.getPrecio() + producto2.getCantidad()*producto2.getPrecio()));
-				
-				System.out.println("*************************************************************************************");
 
 			
 		}
 		
 		
 		
+	}
+
+	private static void imprimirFinal(Producto producto1,double subtotal) {
+		System.out.println("-------------------------------------------------------------------------------------");
+		
+		System.out.println("IVA: 21%");
+		System.out.println("Subtotal (sin IVA):"+ subtotal );
+		System.out.println("Total:"+ subtotal * 0.21  );
+		
+		System.out.println("*************************************************************************************");
+	}
+
+	private static void imprimirDos(Producto producto1) {
+		System.out.println(+producto1.getCantidad() +"      | "+producto1.getDescripcion()+"  		    |  "+producto1.getPrecio()+"			  |  "+producto1.getCantidad()*producto1.getPrecio());
+		
+		System.out.println("-------------------------------------------------------------------------------------");
+	}
+
+	private static void imprimirPrimero(Comprador unComprador, String dia, String mes, String annio) {
+		System.out.println("*************************************************************************************");
+		
+		System.out.println("Fecha:"+dia+"-"+mes+"-"+annio);
+		
+		System.out.println("*************************************************************************************");
+		
+		System.out.println("Nombre: "+unComprador.getRazonSocial());
+		System.out.println("Domicilio: "+unComprador.getDomicilio());
+		
+		System.out.println("*************************************************************************************");
+		
+		System.out.println("Cant.  | Descripcion		|P.unit					 |P.total");
+		System.out.println("-------------------------------------------------------------------------------------");
+	}
+
+	private static void procesarVentas(Scanner scan, Producto producto1) {
+		boolean seguirProcesando = true;
+		
+		double montoProducto, subtotal=0;
+		
+		
+		while( seguirProcesando ) {
+			
+			leerProducto(scan, producto1);
+			
+			 montoProducto = producto1.getCantidad()*producto1.getPrecio();
+			
+			 subtotal = subtotal + montoProducto;
+			 
+			//finaliza el while si la cantidad de productos es igual a 0
+			if(producto1.getCantidad() == 0) {
+				seguirProcesando = false;
+				
+			}imprimirDos(producto1);
+			
+		}imprimirFinal(producto1,subtotal);
 	}
 
 	private static void leerProducto(Scanner scan, Producto producto1) {
